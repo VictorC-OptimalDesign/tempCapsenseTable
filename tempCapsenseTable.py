705546b1523c71c430c19c62d08e934cdfcfdaee
victorc <victorc@optimaldesignco.com>
2022-10-10 10:36:55 -0500

    Initialized python file template. Made a VS code workspace.

---
+++ tempCapsenseTable.py
@@ -0,0 +1,27 @@
+# === IMPORTS ==================================================================
+import enum
+import glob
+import math
+import operator
+import os
+import shutil
+import string
+import typing
+import xlsxwriter
+
+# === GLOBAL CONSTANTS =========================================================
+
+# === FUNCTIONS ================================================================
+
+
+def __process():
+    print('do something')
+
+
+# === MAIN =====================================================================
+
+if __name__ == "__main__":
+    __process()
+else:
+    print("ERROR: tempCapsenseTable needs to be the calling python module!")
+    
